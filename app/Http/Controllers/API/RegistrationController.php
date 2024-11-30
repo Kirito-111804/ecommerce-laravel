13f21f083c4474822218229f976b567b6ee11d7b
Avila2203397 <avilajohnandrei97@gmail.com>
2024-11-30 23:58:37 +0800

    Create RegistrationController.php

---
+++ app/Http/Controllers/API/RegistrationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegistrationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'contact' => 'required|string|max:20', // Fixed field name
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'contact_information' => $request->contact, // Adjusted field name
+        ]);
+
+        return response()->json(['message' => 'User registered successfully!', 'user' => $user], 201);
+    }
+}
